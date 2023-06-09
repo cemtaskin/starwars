d194d1a034531219f59a59ce3980f76d649253f0
cemtaskin <cemtaskin82@gmail.com>
2023-06-09 10:31:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+
 
 function App() {
+
+  const[films,setFilms] = useState([]);
+
+  useEffect(()=>{
+    fetchMoviesHandler();
+  },[]);
+
+  
+
+  function fetchMoviesHandler (){
+  fetch ('https://swapi.dev/api/films').then(
+    response =>{
+      return response.json();
+    }).then(data=>{
+      console.log(data.results);
+      setFilms(data.results);
+
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      {
+        films && films.map(film=>
+          <div key={films.title}>
+            <h2>{film.title}</h2>
+            <h2>{film.created}</h2>
+          </div>
+        )
+      }
     </div>
   );
 }
